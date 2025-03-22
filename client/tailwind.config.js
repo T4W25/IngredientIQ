d9ea6971e00de90b4f9fdb8b8e4f683b3f946527
DhruvSandhar <dhruvsandhar13@gmail.com>
2025-03-21 21:47:29 -0400

    changes

---
+++ client/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"], // ✅ Correct content paths
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
