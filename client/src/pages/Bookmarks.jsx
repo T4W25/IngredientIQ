59b501bef0003aaef2a69bd6df9be60f45ea9a9d
DhruvSandhar <dhruvsandhar13@gmail.com>
2025-04-09 20:33:34 -0400

    updated

--- client/src/pages/Bookmarks.jsx
+++ client/src/pages/Bookmarks.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import API_BASE_URL from "../../api/api";
 
 const Bookmarks = () => {
   const [bookmarks, setBookmarks] = useState([]);
