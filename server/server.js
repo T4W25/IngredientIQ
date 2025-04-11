4429017e69deebaa3c9a05a0f141a417bca2b1de
DhruvSandhar <dhruvsandhar13@gmail.com>
2025-04-10 22:33:55 -0400

    updated

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const moderatorRoutes = require('./routes/moderatorRoutes');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 
 // CORS setup for allowing frontend access
 app.use(cors({
